709ea37c56c4938911854d438d07025511da6a4a
Beartil Rajan <229802592+beartilrajan@users.noreply.github.com>
2025-10-05 14:25:31 +0000

    Grid added and logic added

---
+++ main.js
@@ -0,0 +1,44 @@
+const container = document.querySelector(".container");
+
+let drawing = false;
+document.addEventListener("mousedown", () => drawing = true);
+document.addEventListener("mouseup", () => drawing = false);
+
+
+function grid() {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    const squares = document.querySelectorAll(".square");
+
+    div.addEventListener("mousedown", function () {
+        div.style.background = "red";
+
+    });
+
+
+    div.addEventListener("mouseover", function () {
+        if (drawing) {
+
+            div.style.background = "red";
+        }
+    });
+
+    container.append(div);
+
+}
+
+
+
+
+
+
+for (let i = 1; i <= 16 * 16; i++) {
+    grid();
+}
+
+/*     
+
+    div.addEventListener("mouseover",function(){
+        
+    });
+ */
